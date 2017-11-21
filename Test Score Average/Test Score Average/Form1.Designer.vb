b65dd049027db7fb0280d042e6b70a6c24aa4241
Christine Lewis <33032693+cl131301@users.noreply.github.com>
2017-11-21 09:40:45 -0600

    Updated Test score average

---
+++ Test Score Average/Test Score Average/Form1.Designer.vb
@@ -0,0 +1,180 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.dblScore3 = New System.Windows.Forms.TextBox()
+        Me.dblScore2 = New System.Windows.Forms.TextBox()
+        Me.dblScore1 = New System.Windows.Forms.TextBox()
+        Me.dblAverage = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.dblScore3)
+        Me.GroupBox1.Controls.Add(Me.dblScore2)
+        Me.GroupBox1.Controls.Add(Me.dblScore1)
+        Me.GroupBox1.Controls.Add(Me.dblAverage)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 34)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(376, 237)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Enter Three Test Scores"
+        '
+        'dblScore3
+        '
+        Me.dblScore3.Location = New System.Drawing.Point(146, 145)
+        Me.dblScore3.Name = "dblScore3"
+        Me.dblScore3.Size = New System.Drawing.Size(100, 20)
+        Me.dblScore3.TabIndex = 7
+        '
+        'dblScore2
+        '
+        Me.dblScore2.Location = New System.Drawing.Point(146, 98)
+        Me.dblScore2.Name = "dblScore2"
+        Me.dblScore2.Size = New System.Drawing.Size(100, 20)
+        Me.dblScore2.TabIndex = 6
+        '
+        'dblScore1
+        '
+        Me.dblScore1.Location = New System.Drawing.Point(146, 52)
+        Me.dblScore1.Name = "dblScore1"
+        Me.dblScore1.Size = New System.Drawing.Size(100, 20)
+        Me.dblScore1.TabIndex = 5
+        '
+        'dblAverage
+        '
+        Me.dblAverage.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.dblAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.dblAverage.Location = New System.Drawing.Point(146, 185)
+        Me.dblAverage.Name = "dblAverage"
+        Me.dblAverage.Size = New System.Drawing.Size(100, 23)
+        Me.dblAverage.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(56, 186)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(50, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Average:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(59, 148)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Score 3:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(59, 101)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Score 2:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(59, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Score 1:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(74, 300)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 45)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Calculate Average"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(250, 300)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Clear"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(250, 329)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Exit"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.ClientSize = New System.Drawing.Size(400, 370)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Test Score Average"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents dblScore3 As System.Windows.Forms.TextBox
+    Friend WithEvents dblScore2 As System.Windows.Forms.TextBox
+    Friend WithEvents dblScore1 As System.Windows.Forms.TextBox
+    Friend WithEvents dblAverage As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+
+End Class
---
+++ Test Score Average/Test Score Average/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Variables to hold scores and the average score
+        Dim dblScore1 As Double
+        Dim dblScore2 As Double
+        Dim dblScore3 As Double
+        Dim dblAverage As Double
+
+        'Constants
+        ConstNUM()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles dblScore1.TextChanged
+
+    End Sub
+End Class
