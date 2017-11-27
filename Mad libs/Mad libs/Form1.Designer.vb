a4948d7f0a6f0bebf8f45f496537f7c080a5d12c
Christine Lewis <33032693+cl131301@users.noreply.github.com>
2017-11-27 11:18:54 -0600

    Updated mad libs

---
+++ Mad libs/Mad libs/Form1.Designer.vb
@@ -0,0 +1,267 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.listColor = New System.Windows.Forms.ListBox()
+        Me.listEst = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.listNumer = New System.Windows.Forms.ListBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.listBodyPart = New System.Windows.Forms.ListBox()
+        Me.listAnimal = New System.Windows.Forms.ListBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.listNoun = New System.Windows.Forms.ListBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.listPluralnoun = New System.Windows.Forms.ListBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'listColor
+        '
+        Me.listColor.FormattingEnabled = True
+        Me.listColor.Items.AddRange(New Object() {"Blue", "Red", "Yellow", "Green", "Pink"})
+        Me.listColor.Location = New System.Drawing.Point(12, 51)
+        Me.listColor.Name = "listColor"
+        Me.listColor.Size = New System.Drawing.Size(120, 95)
+        Me.listColor.TabIndex = 0
+        '
+        'listEst
+        '
+        Me.listEst.FormattingEnabled = True
+        Me.listEst.Items.AddRange(New Object() {"Smallest", "Largest", "Smartest", "Fatest", "Tallest", "Shortest"})
+        Me.listEst.Location = New System.Drawing.Point(175, 51)
+        Me.listEst.Name = "listEst"
+        Me.listEst.Size = New System.Drawing.Size(120, 95)
+        Me.listEst.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(36, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Color"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(172, 35)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(121, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Words ending in Est"
+        '
+        'listNumer
+        '
+        Me.listNumer.FormattingEnabled = True
+        Me.listNumer.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
+        Me.listNumer.Location = New System.Drawing.Point(343, 51)
+        Me.listNumer.Name = "listNumer"
+        Me.listNumer.Size = New System.Drawing.Size(120, 95)
+        Me.listNumer.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(369, 37)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(50, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Number"
+        '
+        'listBodyPart
+        '
+        Me.listBodyPart.FormattingEnabled = True
+        Me.listBodyPart.Items.AddRange(New Object() {"Arms", "Legs", "Toes", "Fingers", "Hips", "Heads", "Lips", "Eyes"})
+        Me.listBodyPart.Location = New System.Drawing.Point(15, 189)
+        Me.listBodyPart.Name = "listBodyPart"
+        Me.listBodyPart.Size = New System.Drawing.Size(120, 95)
+        Me.listBodyPart.TabIndex = 6
+        '
+        'listAnimal
+        '
+        Me.listAnimal.FormattingEnabled = True
+        Me.listAnimal.Items.AddRange(New Object() {"Dog", "Cat", "Horse", "Turtle", "Duck", "Chicken", "Squirrel", "Cow", "Pig"})
+        Me.listAnimal.Location = New System.Drawing.Point(175, 189)
+        Me.listAnimal.Name = "listAnimal"
+        Me.listAnimal.Size = New System.Drawing.Size(120, 95)
+        Me.listAnimal.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 173)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(62, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Body Part"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(172, 173)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(44, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Animal"
+        '
+        'listNoun
+        '
+        Me.listNoun.FormattingEnabled = True
+        Me.listNoun.Items.AddRange(New Object() {"Boat", "House", "Car", "School", "City", "Desk", "Bed", "Table"})
+        Me.listNoun.Location = New System.Drawing.Point(343, 189)
+        Me.listNoun.Name = "listNoun"
+        Me.listNoun.Size = New System.Drawing.Size(120, 95)
+        Me.listNoun.TabIndex = 10
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(340, 173)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(37, 13)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Noun"
+        '
+        'listPluralnoun
+        '
+        Me.listPluralnoun.FormattingEnabled = True
+        Me.listPluralnoun.Items.AddRange(New Object() {"Lunches", "Moons", "Schools", "Churches", "Kids", "Families", "Phones"})
+        Me.listPluralnoun.Location = New System.Drawing.Point(12, 323)
+        Me.listPluralnoun.Name = "listPluralnoun"
+        Me.listPluralnoun.Size = New System.Drawing.Size(120, 95)
+        Me.listPluralnoun.TabIndex = 12
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(12, 307)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(73, 13)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "Plural Noun"
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSubmit.Location = New System.Drawing.Point(175, 323)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(133, 41)
+        Me.btnSubmit.TabIndex = 14
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(330, 323)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(133, 41)
+        Me.btnExit.TabIndex = 15
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(175, 377)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(133, 41)
+        Me.btnClear.TabIndex = 16
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Palatino Linotype", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(155, 9)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(172, 26)
+        Me.Label8.TabIndex = 17
+        Me.Label8.Text = "Dragon Story Time"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Plum
+        Me.ClientSize = New System.Drawing.Size(498, 460)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.listPluralnoun)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.listNoun)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.listAnimal)
+        Me.Controls.Add(Me.listBodyPart)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.listNumer)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.listEst)
+        Me.Controls.Add(Me.listColor)
+        Me.Name = "Form1"
+        Me.Text = "Color"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents listColor As System.Windows.Forms.ListBox
+    Friend WithEvents listEst As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents listNumer As System.Windows.Forms.ListBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents listBodyPart As System.Windows.Forms.ListBox
+    Friend WithEvents listAnimal As System.Windows.Forms.ListBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents listNoun As System.Windows.Forms.ListBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents listPluralnoun As System.Windows.Forms.ListBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents btnSubmit As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+
+End Class
---
+++ Mad libs/Mad libs/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Dim strInput As String 'Hold selected from list boxes
+
+        If listColor.SelectedIndex = -1 Then
+            MessageBox.Show("select a color.")
+        ElseIf listEst.SelectedIndex = -1 Then
+            MessageBox.Show("No word ending in EST was selceted.")
+        End If
+
+        strInput = listColor.SelectedItem.ToString() & " Dragon is the " &
+            listEst.SelectedItem.ToString() & " Dragon of all. It has " & listNumer.SelectedItem.ToString() &
+            " " & listBodyPart.SelectedItem.ToString() & ", and a " &
+            listAnimal.SelectedItem.ToString() & " Shaped like a " &
+            listNoun.SelectedItem.ToString() & ", it loves to eat " &
+            listPluralnoun.SelectedItem.ToString()
+
+        MessageBox.Show("The " & strInput & ", although it will feast on nearly anything.")
+
+
+
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        listColor.SelectedIndex = -1
+        listAnimal.SelectedIndex = -1
+        listBodyPart.SelectedIndex = -1
+        listEst.SelectedIndex = -1
+        listNoun.SelectedIndex = -1
+        listPluralnoun.SelectedIndex = -1
+        listNumer.SelectedIndex = -1
+    End Sub
+End Class
