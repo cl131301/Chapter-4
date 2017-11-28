73e4514227284abb6b43afe86d6eb7c4c451299d
Christine Lewis <33032693+cl131301@users.noreply.github.com>
2017-11-28 10:26:28 -0600

    Updated hello world password

---
+++ hello world password/hello world password/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtboxPassword = New System.Windows.Forms.TextBox()
+        Me.btnPassword = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe Marker", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(35, 62)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(279, 24)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Please enter correct passoword"
+        '
+        'txtboxPassword
+        '
+        Me.txtboxPassword.Location = New System.Drawing.Point(39, 105)
+        Me.txtboxPassword.Name = "txtboxPassword"
+        Me.txtboxPassword.Size = New System.Drawing.Size(275, 20)
+        Me.txtboxPassword.TabIndex = 1
+        '
+        'btnPassword
+        '
+        Me.btnPassword.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPassword.Location = New System.Drawing.Point(111, 147)
+        Me.btnPassword.Name = "btnPassword"
+        Me.btnPassword.Size = New System.Drawing.Size(122, 61)
+        Me.btnPassword.TabIndex = 2
+        Me.btnPassword.Text = "Try password"
+        Me.btnPassword.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(111, 230)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(122, 54)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(346, 347)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnPassword)
+        Me.Controls.Add(Me.txtboxPassword)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtboxPassword As System.Windows.Forms.TextBox
+    Friend WithEvents btnPassword As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ hello world password/hello world password/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub btnPassword_Click(sender As Object, e As EventArgs) Handles btnPassword.Click
+        If txtboxPassword.Text = "Lewis123" Then
+            MessageBox.Show("Good Morning World!")
+        Else
+            MessageBox.Show("Incorrect assword, please enter valid password")
+        End If
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
