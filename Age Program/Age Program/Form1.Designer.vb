7429d6345f1cb04c25252fbf0be61993314458e0
Christine Lewis <33032693+cl131301@users.noreply.github.com>
2017-11-28 10:56:34 -0600

    Updated Age Program

---
+++ Age Program/Age Program/Form1.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtUserAgeInput = New System.Windows.Forms.TextBox()
+        Me.btnInsult = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.GroupBox1.Controls.Add(Me.lblMessage)
+        Me.GroupBox1.Controls.Add(Me.lblResult)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtUserAgeInput)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 53)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(364, 229)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'lblMessage
+        '
+        Me.lblMessage.AutoSize = True
+        Me.lblMessage.Location = New System.Drawing.Point(106, 119)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(36, 13)
+        Me.lblMessage.TabIndex = 3
+        Me.lblMessage.Text = "Resilt:"
+        '
+        'lblResult
+        '
+        Me.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblResult.Location = New System.Drawing.Point(171, 104)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(100, 50)
+        Me.lblResult.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(86, 64)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Input Age:"
+        '
+        'txtUserAgeInput
+        '
+        Me.txtUserAgeInput.Location = New System.Drawing.Point(171, 64)
+        Me.txtUserAgeInput.Name = "txtUserAgeInput"
+        Me.txtUserAgeInput.Size = New System.Drawing.Size(100, 20)
+        Me.txtUserAgeInput.TabIndex = 0
+        '
+        'btnInsult
+        '
+        Me.btnInsult.Location = New System.Drawing.Point(26, 317)
+        Me.btnInsult.Name = "btnInsult"
+        Me.btnInsult.Size = New System.Drawing.Size(75, 23)
+        Me.btnInsult.TabIndex = 1
+        Me.btnInsult.Text = "Insult"
+        Me.btnInsult.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(154, 317)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 2
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(287, 317)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(388, 380)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnInsult)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents lblMessage As System.Windows.Forms.Label
+    Friend WithEvents lblResult As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtUserAgeInput As System.Windows.Forms.TextBox
+    Friend WithEvents btnInsult As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Age Program/Age Program/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+
+    Private Sub btnInsult_Click(sender As Object, e As EventArgs) Handles btnInsult.Click
+        Dim intAge As Double
+
+        Try
+            intAge = txtUserAgeInput.Text
+
+            If intAge < 10 Then
+                lblResult.Text = "Can you even read?"
+            ElseIf intAge >= 10 And intAge < 14 Then
+                lblResult.Text = "There are no words to express how naive you are."
+            ElseIf intAge <= 14 And intAge < 18 Then
+                lblResult.Text = "You have no clue how the real world works."
+            ElseIf intAge >= 18 And intAge < 24 Then
+                lblResult.Text = "You are less advanced then all of your peers."
+            ElseIf intAge >= 24 And intAge < 30 Then
+                lblResult.Text = "Get a job you deadbeat."
+            ElseIf intAge >= 30 And intAge < 40 Then
+                lblResult.Text = "You are slowly dying."
+            ElseIf intAge >= 40 And intAge < 60 Then
+                lblResult.Text = "Your grandkids don't love you."
+            ElseIf intAge >= 60 Then
+                lblResult.Text = "You have wrinkles."
+            End If
+        Catch ex As Exception
+            lblMessage.Text = "Enter a numeric value."
+        End Try
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtUserAgeInput.Clear()
+        lblResult.Text = String.Empty
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
